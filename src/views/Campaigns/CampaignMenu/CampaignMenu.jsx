d9b3480f1226de32d897d1049b1e4492fa6f7daa
MarkNaArea <127256808+MarkNaArea@users.noreply.github.com>
2024-01-16 23:59:56 -0300

    Starting campaign menu

--- src/views/Campaigns/CampaignMenu/CampaignMenu.jsx
+++ src/views/Campaigns/CampaignMenu/CampaignMenu.jsx
@@ -1,3 +1,20 @@
+import { ScrollView, View } from "react-native"
+import { globalStyles } from "../../../styles/global"
+
 export const CampaignMenu = ({route, navigation}) => {
-    
+    const [campaign, setCampaign] = useState([])
+
+    const fetchCampaign = async (campaignid) => {
+        setCampaign(await getCampanhaById(campaignid))
+    }
+
+    useEffect(() => {
+        fetchCampaign(route.params.campaignid)
+    }, [])
+
+    return (
+        <ScrollView>
+            <View style={globalStyles.card}></View>
+        </ScrollView>
+    )
 }
