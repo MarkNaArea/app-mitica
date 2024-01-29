c9f410ba8b4b45f1c0cd2f167bc5bcd47e796ab4
MarkNaArea <127256808+MarkNaArea@users.noreply.github.com>
2024-01-28 23:27:48 -0300

    Class controllers

---
+++ src/apiHelper/classControllers.js
@@ -0,0 +1,42 @@
+export const getClasses = async () => {
+    return fetch(process.env.BASE_URL + "/class/", { method: "GET" })
+        .then(response => {
+            if (response.status === 200 || response.status === 304) {
+                return response.json();
+            } else {
+                console.log(response)
+                throw new Error("Erro ao buscar classe");
+            }
+        })
+        .then(responseJson => responseJson);
+};
+
+export const getClassById = async (id) => {
+    return fetch(process.env.BASE_URL + "/class/?id=" + id, { method: "GET" })
+        .then((response) => {
+            if (response.status === 200 || response.status === 304) {
+                return response.json();
+            } else {
+                throw new Error("Erro ao buscar classe");
+            }
+        })
+        .then((responseJson) => responseJson);
+};
+
+export const postClass = async (itemClass) => {
+    return fetch(process.env.BASE_URL + "/class/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(campanha)
+    })
+        .then((response) => {
+            if (response.status === 200 || response.status === 304) {
+                return response.json();
+            } else {
+                throw new Error("Erro ao buscar classe");
+            }
+        })
+        .then((responseJson) => responseJson);
+};
